473349ba21f4a1216f8fa215f29161fbf42dd486
Luca Baldesi <l.baldesi@northeastern.edu>
2021-04-19 10:24:24 -0400

    refactor convolution/pooling and add skip-connection link

--- brain.py
+++ brain.py
@@ -4,19 +4,27 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def conv_out_n(inputs, kernel_size, padding, dilation=1, stride=1):
+def conv_out_n(inputs, kernel_size, padding, stride, dilation=1):
+    '''
+    Compute the output size of a convolution or pool torch function.
+    For convolution, stride should default to 1, for pooling, to kernel_size
+    '''
     outs = inputs + 2*padding - dilation*(kernel_size-1) - 1
     outs = outs/stride + 1
     outs = math.floor(outs)
     return int(outs)
 
 
-def k_conv_out_n(k, inputs, kernel_size, padding, dilation=1, stride=1):
+def k_conv_out_n(k, inputs, kernel_size, pool_kernel_size, padding):
+    '''
+    Compute the output size of a series of convolution (and optionally pooling) layers).
+    '''
     n = 0
     out = inputs
     for i in range(k):
-        out = conv_out_n(out, kernel_size, padding, dilation, stride)
-        out = math.floor(out/kernel_size)
+        out = conv_out_n(out, kernel_size, padding, 1)
+        if pool_kernel_size > 0:
+            out = conv_out_n(out, pool_kernel_size, 0, pool_kernel_size)
     return out
 
 
@@ -27,12 +35,13 @@ def params_count(model):
 class CharmBrain(nn.Module):
     def __init__(self, chunk_size=20000):
         super().__init__()
-        chs = 4  # channels
-        self.cl1 = nn.Conv1d(2, chs, kernel_size=11)
-        self.cl2 = nn.Conv1d(chs, chs, kernel_size=11)
-        self.cl3 = nn.Conv1d(chs, chs, kernel_size=11)
+        chs = 4  # convolution output channels
 
-        self.ll1_n = k_conv_out_n(3, chunk_size, 11, 0)*chs
+        self.cl1 = nn.Conv1d(2, chs, kernel_size=11, padding=5)
+        self.cl2 = nn.Conv1d(chs, chs, kernel_size=11, padding=5)
+        self.cl3 = nn.Conv1d(chs, chs, kernel_size=11, padding=5)
+
+        self.ll1_n = k_conv_out_n(3, chunk_size, 11, 10, 5)*chs
         self.ll2_n = 32
 
         self.fc1 = nn.Linear(self.ll1_n, self.ll2_n)
@@ -42,9 +51,10 @@ class CharmBrain(nn.Module):
 
     def forward(self, x):
         y = x
-        y = F.max_pool1d(torch.tanh(self.cl1(y)), 11)
-        y = F.max_pool1d(torch.tanh(self.cl2(y)), 11)
-        y = F.max_pool1d(torch.tanh(self.cl3(y)), 11)
+        y = F.max_pool1d(torch.tanh(self.cl1(y)), 10)
+        y = F.max_pool1d(torch.tanh(self.cl2(y)), 10)
+        y1 = y
+        y = F.max_pool1d(torch.tanh(self.cl3(y)) + y1, 10)
         y = y.view(-1, self.ll1_n)
         y = torch.tanh(self.fc1(y))
         y = self.fc2(y)
