04b13b2fd9176ac70e11cdbc82d2d97dec717e90
Luca Baldesi <l.baldesi@northeastern.edu>
2021-04-22 11:57:03 -0400

    add resnet arch

--- brain.py
+++ brain.py
@@ -35,27 +35,60 @@ def params_count(model):
 class CharmBrain(nn.Module):
     def __init__(self, chunk_size=20000):
         super().__init__()
-        chs = 10  # convolution output channels
+        chs = 32  # convolution output channels
 
-        self.cl1 = nn.Conv1d(2, chs, kernel_size=11, padding=5)
-        self.cl2 = nn.Conv1d(chs, chs, kernel_size=11, padding=5)
-        self.cl3 = nn.Conv1d(chs, chs, kernel_size=11, padding=5)
+        self.cl0 = nn.Conv1d(2, 2, kernel_size=11, padding=5)
+        self.cl1 = nn.Conv1d(2, chs, kernel_size=3, padding=1)
+        self.cl2 = nn.Conv1d(chs, chs, kernel_size=3, padding=1)
+        self.cl3 = nn.Conv1d(chs, chs, kernel_size=3, padding=1)
+        self.cl4 = nn.Conv1d(chs, chs, kernel_size=3, padding=1)
+        self.cl5 = nn.Conv1d(chs, chs, kernel_size=3, padding=1)
+        self.cl6 = nn.Conv1d(chs, chs, kernel_size=3, padding=1)
 
-        self.ll1_n = k_conv_out_n(3, chunk_size, 11, 10, 5)*chs
-        self.ll2_n = 10
+        self.bn0 = nn.BatchNorm1d(num_features=2)
+        self.bn1 = nn.BatchNorm1d(num_features=chs)
+        self.bn2 = nn.BatchNorm1d(num_features=chs)
+        self.bn3 = nn.BatchNorm1d(num_features=chs)
+        self.bn4 = nn.BatchNorm1d(num_features=chs)
+        self.bn5 = nn.BatchNorm1d(num_features=chs)
+        self.bn6 = nn.BatchNorm1d(num_features=chs)
+
+        self.ll1_n = k_conv_out_n(1, chunk_size, 11, 10, 5)
+        self.ll1_n = k_conv_out_n(6, self.ll1_n, 3, 2, 1)*chs
+        self.ll2_n = 12
+        self.ll3_n = 12
+
+        self.dpout1 = nn.Dropout(p=0.0)
+        self.dpout2 = nn.Dropout(p=0.0)
+        self.dpout3 = nn.Dropout(p=0.0)
 
         self.fc1 = nn.Linear(self.ll1_n, self.ll2_n)
-        self.fc2 = nn.Linear(self.ll2_n, 3)
+        self.fc2 = nn.Linear(self.ll2_n, self.ll3_n)
+        self.fc3 = nn.Linear(self.ll3_n, 3)
         print(f"Inner nodes: {self.ll1_n}")
         print(f"Parameters: {params_count(self)}")
 
     def forward(self, x):
         y = x
-        y = F.max_pool1d(torch.tanh(self.cl1(y)), 10)
-        y = F.max_pool1d(torch.tanh(self.cl2(y)), 10)
+        y = F.max_pool1d(torch.relu((self.cl0(y))), 10)
+        y = F.max_pool1d(torch.relu((self.cl1(y))), 2)
         y1 = y
-        y = F.max_pool1d(torch.tanh(self.cl3(y)) + y1, 10)
+        y = F.max_pool1d(torch.relu((self.cl2(y)))+y1, 2)
+        y1 = y
+        y = F.max_pool1d(torch.relu((self.cl3(y)))+y1, 2)
+        y1 = y
+        y = F.max_pool1d(torch.relu((self.cl4(y)))+y1, 2)
+        y1 = y
+        y = F.max_pool1d(torch.relu((self.cl5(y)))+y1, 2)
+        y1 = y
+        y = F.max_pool1d(torch.relu((self.cl6(y)))+y1, 2)
+
         y = y.view(-1, self.ll1_n)
-        y = torch.tanh(self.fc1(y))
-        y = self.fc2(y)
+        y = self.dpout1(y)
+        y = torch.selu(self.fc1(y))
+        y = self.dpout2(y)
+        y = torch.selu(self.fc2(y))
+        y = self.dpout3(y)
+        y = torch.selu(self.fc3(y))
+
         return y
